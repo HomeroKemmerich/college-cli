95dff157794b24910f0894990dc3c206df805d59
HomeroKemmerich <146676@upf.br>
2024-05-06 12:10:03 -0300

    scripts

---
+++ scripts/windows/new_preview.bat
@@ -0,0 +1,18 @@
+@echo off
+REM Obter a data atual
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set datetime=%%I
+
+REM Extrair ano, mês e dia do datetime
+set year=%datetime:~0,4%
+set month=%datetime:~4,2%
+set day=%datetime:~6,2%
+
+REM Criar o diretório do novo arquivo
+mkdir .\Classes\%year%%month%%day%_preview
+cd .\Classes\%year%%month%%day%_preview
+
+REM Criar o arquivo README.md
+echo # %1 > README.md
+
+
+
